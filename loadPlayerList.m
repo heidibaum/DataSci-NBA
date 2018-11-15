f8d023468b9814c3bf0236d4bc1d9afd22d17af8
heidibaum <38989078+heidibaum@users.noreply.github.com>
2018-11-14 16:16:56 -0800

    Add files via upload

--- loadPlayerList.m
+++ loadPlayerList.m
@@ -6,13 +6,13 @@ function playerList = loadPlayerList(n)
 %% Read the fixed player list
 fid = fopen('players.txt');
 
-playerList = cell(n, 3);
+playerList = cell(n, 4);
 for i = 1:n
     % Read line
     line = fgets(fid);
     % segment player name and team and add to list
     playerList(i,:) = strsplit(line,',');
-    
+
 end
 fclose(fid);
 
